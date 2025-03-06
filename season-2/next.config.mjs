2bd4c4a4f83b9db153e8867004f5c795cff94c51
git <frankenstein0671@gmail.com>
2025-03-06 15:42:46 +0530

    Render

--- season-2/next.config.mjs
+++ season-2/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devIndicators:false
-};
-
-export default nextConfig;
+    devIndicators: false,
+    output: 'standalone', // Ensures all dependencies are bundled
+  };
+  
+  export default nextConfig;
